51a92fa09a2b62685d294c353a0a29d1f1e108a7
zziyuni <xhrl6671@gmail.com>
2022-07-15 02:46:32 +0900

    feat: add screens

---
+++ lib/signUp.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+// ignore_for_file: prefer_const_constructors
+
+class SignUp extends StatefulWidget{
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body:Center(
+        child: SingleChildScrollView(child: 
+        Container(
+          width: 360,
+          height: 600,
+            decoration: new BoxDecoration(
+    borderRadius: new BorderRadius.circular(16.0),
+    color: const Color(0xffF0EAD2),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 3), // changes position of shadow
+      ),
+    ],
+    
+  ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Text('Sign Up',style:TextStyle(fontSize: 20,fontFamily: 'YS',fontWeight: FontWeight.bold,color: const Color(0xff6C584C))),
+           
+             Image(
+                  image: (AssetImage('src/images/logo_2.png')),
+                  width: 160.0,
+                  height: 180.0,
+                ),
+              
+             Form(
+                child: Theme(
+                  data: ThemeData(
+                    primaryColor: const Color(0xff6C584C),
+                    fontFamily: 'NanumSquare',
+                  ),
+                  child: Container(
+                        width: 270,
+                        child: Column(children: [
+                          signupField("Email", false,Icon(Icons.mail_outline, color: const Color(0xff6C584C),)),
+                             Padding(padding: EdgeInsets.only(top: 8)),
+                          signupField("Password", true,Icon(Icons.password, color: const Color(0xff6C584C),)),
+                             Padding(padding: EdgeInsets.only(top: 8)),
+                          signupField("Confirm Password", true,Icon(Icons.password, color: const Color(0xff6C584C),)),
+            
+                          Padding(padding: EdgeInsets.only(top: 20)),
+                          ElevatedButton(
+                            onPressed: (){
+                              Navigator.pop(context);
+                            },
+                            child: Text('회원가입'),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(160, 45),
+                              maximumSize: Size(160, 45),
+                              elevation: 5,
+                              primary: Color(0xffADC178),
+                              onPrimary: Colors.white,
+                              textStyle: const TextStyle(
+                                  fontSize: 26, fontWeight: FontWeight.bold),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18)),
+                            ),
+                          )
+                        ]),
+                      ),
+                ),
+              )
+          ]),
+        ),),
+      )
+    );
+  }
+    TextField signupField(String text, bool b,Icon i) {
+          return TextField(
+    
+      cursorColor:const Color(0xff6C584C),
+      style: TextStyle(height: 1.5),
+      decoration: InputDecoration(
+        prefixIcon: i,
+        border: OutlineInputBorder(borderRadius:BorderRadius.circular(15.0),
+      borderSide:BorderSide(color: const Color(0xff6C584C), 
+    ),
+    ),
+    focusedBorder: OutlineInputBorder(borderRadius:BorderRadius.circular(15.0),
+      borderSide:BorderSide(color: const Color(0xff6C584C), width: 2.0
+    ),
+    ),
+          filled: true,
+          fillColor: Colors.white70,
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+          labelText: text,
+          labelStyle: TextStyle(
+              color:const Color(0xff6C584C)
+                 )),
+                  
+      keyboardType: TextInputType.text,
+      obscureText: b,
+    );
+
+
+  }
+}
+
+
+
---
+++ lib/splashScreen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:symda/loginPage.dart';
+//ignore_for_file: prefer_const_constructors
+
+class SplashScreen extends StatefulWidget{
+
+ 
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin  {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+@override
+  void initState() {
+  	Timer(Duration(milliseconds: 2500), () {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => LogIn()
+                  )
+                  );
+                });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+final String imageLogoName = 'src/images/logo.png';
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+     return WillPopScope(
+      onWillPop: () async => false,
+      child: MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor:1.0),
+        child: new Scaffold(
+          backgroundColor: const Color(0xffF0EAD2),
+          body: Center(
+            child: ScaleTransition(
+              scale: _animation,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                 Image.asset(
+                      imageLogoName,
+                      width: screenWidth * 0.6,
+                      height: screenHeight * 0.6,
+                    ),
+                ],
+              ),
+            ),
+
+          ),
+        ),
+      ),
+    );
+}
+}
