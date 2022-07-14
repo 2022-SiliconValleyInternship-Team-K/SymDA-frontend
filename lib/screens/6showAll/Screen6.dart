ad8a4a9cf3b7c4815e9088591d565db6a05de28b
youngaekim <youngaekim915@gmail.com>
2022-07-14 22:26:16 +0900

    6 일기 모아보기 화면

---
+++ lib/screens/6showAll/Screen6.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Screen6 extends StatelessWidget {
+  const Screen6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Screen 6: 일기모아보기'),
+    );
+  }
+}
