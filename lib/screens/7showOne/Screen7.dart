5a3df0c27b6f32ae1913a40db49b7d61474c7b79
youngaekim <youngaekim915@gmail.com>
2022-07-14 22:26:24 +0900

    7 일기 상세보기 화면

---
+++ lib/screens/7showOne/Screen7.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Screen7 extends StatelessWidget {
+  const Screen7({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Screen 7: 일기상세보기'),
+    );
+  }
+}
