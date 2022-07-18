b736e6838ccbf47486e2c7aea8ea31923e6c7731
youngaekim <youngaekim915@gmail.com>
2022-07-19 03:17:28 +0900

    일기 상세보기 화면

--- lib/screens/7showOne/Screen7.dart
+++ lib/screens/7showOne/Screen7.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:symda_application_1/screens/7showOne/components/emotionHeader.dart';
+import 'package:symda_application_1/screens/7showOne/components/headerText2.dart';
+import 'package:symda_application_1/screens/7showOne/components/showOneBody.dart';
 
 class Screen7 extends StatelessWidget {
   const Screen7({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Screen 7: 일기상세보기'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('일기 상세보기'),
+        ),
+        body: ListView(
+          children: [
+            HeaderText2(),
+            EmotionHeader(),
+            Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
+              child: ShowOneBody(
+                  // showAll: showAllList[0], // 일기가 여러 개이면 여러 개 보여줌
+                  ),
+            )
+          ],
+        ));
   }
 }
