23d0ad916543212b987ad41bc615599a70c9481f
youngaekim <youngaekim915@gmail.com>
2022-07-19 03:17:16 +0900

    일기 모아보기 화면

--- lib/screens/6showAll/Screen6.dart
+++ lib/screens/6showAll/Screen6.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:symda_application_1/screens/6showAll/components/showAllBody.dart';
 
 class Screen6 extends StatelessWidget {
   const Screen6({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Screen 6: 일기모아보기'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('일기 모아보기'),
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
+              child: ShowAllBody(
+                  // showAll: showAllList[0], // 일기가 여러 개이면 여러 개 보여줌
+                  ),
+            )
+          ],
+        ));
   }
 }
