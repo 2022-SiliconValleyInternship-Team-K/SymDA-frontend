a17641f6d15b854bdc91f1c368cf9c40f522aead
youngaekim <youngaekim915@gmail.com>
2022-07-19 03:17:08 +0900

    일기 작성 완료 화면

--- lib/screens/5BwriteDone/Screen5B.dart
+++ lib/screens/5BwriteDone/Screen5B.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:symda_application_1/screens/5Awriting/components/doneButton.dart';
+import 'package:symda_application_1/screens/5Awriting/components/headerText.dart';
+import 'package:symda_application_1/screens/5Awriting/components/weather.dart';
+import 'package:symda_application_1/screens/5BwriteDone/components/doneButton2.dart';
+import 'package:symda_application_1/screens/5BwriteDone/components/writingDoneBody.dart';
 
 class Screen5B extends StatelessWidget {
   const Screen5B({Key? key}) : super(key: key);
@@ -7,14 +12,17 @@ class Screen5B extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Screen5B'),
+        title: Text('일기 작성 완료 화면'),
       ),
       body: Center(
-        child: Text(
-          'Screen5B',
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ),
+          child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+              child: ListView(children: [
+                HeaderText(),
+                Weather(), // 오늘의 날씨는?
+                DoneBody(),
+                DoneButton2(), // 작성 완료 버튼
+              ]))),
     );
   }
 }
