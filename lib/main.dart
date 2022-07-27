657bb5e7bd379e8d24a90e44d479dbeb395dbe6f
youngaekim <youngaekim915@gmail.com>
2022-07-27 15:50:34 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,35 @@ import 'package:symda/pages/emotionSelect.dart';
 import 'package:symda/pages/loginPage.dart';
 import 'package:symda/pages/mainCalender.dart';
 import 'package:symda/pages/plant.dart';
+import 'package:symda/pages/screens/5BwriteDone/Screen5B.dart';
+import 'package:symda/pages/screens/6showAll/Screen6.dart';
+import 'package:symda/pages/screens/7showOne/Screen7.dart';
 import 'package:symda/pages/signUp.dart';
 import 'package:symda/pages/splashScreen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:http/http.dart' as http;
+import 'package:symda/src/theme.dart';
 
-void main(){
-  initializeDateFormatting().then((_)=>runApp(MyApp()));
+void main() {
+  initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
-class MyApp extends StatelessWidget{
-
-  Widget build(BuildContext context){
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:"SYMDA",
-      home:SplashScreen(),
-      routes: <String,WidgetBuilder>{
-        '/login':((BuildContext context) => LogIn()),
-        '/signup':((BuildContext context) => SignUp()),
-        '/EmotionSelect':((BuildContext context) => EmotionSelect()),
-        '/plant':((BuildContext context) => plant()),
-        '/emotion':((BuildContext context) => mainCalender()),
-        
-   
-       
+      title: "SYMDA",
+      home: SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/login': ((BuildContext context) => LogIn()),
+        '/signup': ((BuildContext context) => SignUp()),
+        '/EmotionSelect': ((BuildContext context) => EmotionSelect()),
+        '/plant': ((BuildContext context) => plant()),
+        '/emotion': ((BuildContext context) => mainCalender()),
+        '/writingdone': ((BuildContext context) => Screen5B()),
+        '/showone': ((BuildContext context) => Screen7()),
+        '/showall': ((BuildContext context) => Screen6()),
       },
     );
   }
-
 }
