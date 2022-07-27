3c167cf0d8c082939ef5836835e9829bc609addd
youngaekim <youngaekim915@gmail.com>
2022-07-27 15:58:13 +0900

    푸시8

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\USER\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter\\SymDa-frontend" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\USER\\Downloads\\flutter"
+  "PROJECT_DIR=D:\\flutter\\SymDa-frontend"
+  "FLUTTER_ROOT=C:\\Users\\USER\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter\\SymDa-frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter\\SymDa-frontend"
+  "FLUTTER_TARGET=D:\\flutter\\SymDa-frontend\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter\\SymDa-frontend\\.dart_tool\\package_config.json"
+)
